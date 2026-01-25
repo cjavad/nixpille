e8e49af244f746658c87b7580c556930f90b4f13
Javad Asgari Shafique <me@javad.sh>
2026-01-25 05:21:48 +0100

    Formatting

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -22,10 +22,16 @@
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
       ];
-      experimental-features = [ "nix-command" "flakes" ];
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
       auto-optimise-store = true;
       builders-use-substitutes = true;
-      trusted-users = [ "root" "@wheel" ];
+      trusted-users = [
+        "root"
+        "@wheel"
+      ];
     };
     gc = {
       automatic = true;
@@ -38,7 +44,10 @@
     loader.systemd-boot.configurationLimit = lib.mkDefault 10;
     loader.grub.configurationLimit = lib.mkDefault 10;
     kernelPackages = pkgs.linuxPackages_zen;
-    kernelParams = [ "quiet" "splash" ];
+    kernelParams = [
+      "quiet"
+      "splash"
+    ];
     kernel.sysctl = {
       "vm.swappiness" = 10;
       "vm.vfs_cache_pressure" = 50;
